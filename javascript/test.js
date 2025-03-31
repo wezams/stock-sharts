17c6b0f65173548fddded6153fa8b530e76ef0a7
wezams <148435240+wezams@users.noreply.github.com>
2025-03-31 18:58:27 +0200

    a bit of everything

--- javascript/test.js
+++ javascript/test.js
@@ -1,2 +1,20 @@
-import 'dotenv/config'
-console.log(process.env)
+import dotenv from 'dotenv';
+//import axios from 'axios';
+import {data} from './values.js'
+
+//const company = "BYDDY";
+
+dotenv.config({ path: './secrets/AlphaApi.env' });
+const secretKey = process.env.SECRET_KEY;
+
+console.log(data["Time Series (Daily)"]["2025-03-28"]);
+
+/*
+axios.get('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + company + '&apikey=' + secretKey)
+  .then(response => {
+    console.log(response.data["Time Series (Daily)"]["2025-03-28"]);
+  })
+  .catch(error => {
+    console.error('There was an error with the request:', error);
+  });
+*/
