7e5d42ae5afc4e132a93d03b49a09c9b3b7f7f05
wezams <148435240+wezams@users.noreply.github.com>
2025-05-03 23:20:37 +0200

    now?

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: '/stock-sharts/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        quote: resolve(__dirname, 'quote.html'),
+      }
+    }
+  }
 })
