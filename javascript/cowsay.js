84ce365683cbf9e1ca919ac4b66ecf62b6f56c61
wezams <148435240+wezams@users.noreply.github.com>
2025-05-04 21:41:25 +0200

    cool eyes

--- javascript/cowsay.js
+++ javascript/cowsay.js
@@ -1,4 +1,13 @@
 import { say } from 'cowsay';
+const i = {
+    e: n.eyes || "^^",
+    T: n.tongue || "  ",
+    n: n.wrap,
+    W: n.wrapLength || 40,
+    text: n.text || "",
+    _: n.text || [],
+    f: n.cow
+};
 
 export function HejDanne() {
   console.clear;
